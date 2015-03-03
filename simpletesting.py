7e25c16e3d4d8b5391e5fa6c253d972684ddad4e
Lopsy <linyks@gmail.com>
2015-03-03 12:13:49 +0100

    added weights

--- simpletesting.py
+++ simpletesting.py
@@ -4,37 +4,37 @@ from simple import Copier
 
 
 
-def quickTest(player1, player2, castles, soldiers, iterations):
+def quickTest(player1, player2, weights, soldiers, iterations):
     """Call like
     quickTest(Copier, CleverStrat, 10, 100, 9000)"""
     totalscore = 0 # positive for P1 winning, negative for P2 winning
 
-    P1 = player1(castles, soldiers)
-    P2 = player2(castles, soldiers)
+    P1 = player1(weights, soldiers)
+    P2 = player2(weights, soldiers)
 
     move1 = P1.next()
     move2 = P2.next()
     
     for i in xrange(iterations):
-        totalscore += score(move1, move2, castles, soldiers)
+        totalscore += score(move1, move2, weights, soldiers)
         move1, move2 = P1.send(move2), P2.send(move1)
 
     return totalscore
         
-def score(move1, move2, castles, soldiers):
-    check(move1, castles, soldiers)
-    check(move2, castles, soldiers)
+def score(move1, move2, weights, soldiers):
+    check(move1, weights, soldiers)
+    check(move2, weights, soldiers)
 
     total = 0
-    for i in xrange(castles):
-        if move1[i] > move2[i]: total += 1
-        if move1[i] < move2[i]: total -= 1
+    for i in xrange(len(weights)):
+        if move1[i] > move2[i]: total += weights[i]
+        if move1[i] < move2[i]: total -= weights[i]
 
     return total
 
 
-def check(move, castles, soldiers):
+def check(move, weights, soldiers):
     assert(all(a == int(a) for a in move))
     assert(sum(move) == soldiers)
-    assert(len(move) == castles)
+    assert(len(move) == len(weights))
     move[0]
